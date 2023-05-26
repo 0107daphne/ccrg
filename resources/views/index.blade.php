b7830593075d45c34cd92171d88b4229e24e0c5a
DAPHNE GOH <0107daph@gmail.com>
2023-05-26 18:11:14 +0800

    test for the 1000th time

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,10 @@
     <head>
         <title>AR-community</title>
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        {{-- <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" /> --}}
         <!-- CSS -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <!-- Required meta tags -->
         <meta charset="utf-8">
@@ -15,20 +16,19 @@
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    </head>
+
 
     <body>
         <div class="container">
-            <div class="p-3 mb-2 bg-light text-gray d-flex"></div>
+            <div class="p-3 mb-2 bg-light text-gray d-flex">
                 <div class="w-50">
-                    <img src="{{url('/img/FKI.png') }}" height="80">
+                    <img src="{{url('/img/FKI.png') }}" height="60">
                     <img src="{{url('/img/creativecomputing.png') }}" height="80">
                 </div>
                 <div class="flex-fill">
                     <h2 class="font-weight-light">FCI | CCRG</h2>
                     <h5 class="text-muted">GRANT | PROJECT</h5>
                 </div>
-
             </div>
             <div class="bg-dark-subtle font-weight-bold p-3 mb-2 text-black">
                 <h4>AR with Community</h4>
@@ -40,7 +40,7 @@
                     <h4>AR DanceSabah</h4>
                     <p><a href="https://drive.google.com/file/d/1X3iN1Z60m7Ohk1YTdaQ7GDv7gORlfiJv/view" class="text-secondary">ARdance Privacy Policy</a></p>
                     <p>Youtube link: <a href="https://www.youtube.com/@lkns.project" class="text-muted">youtube.com/@lkns.project</a></p>
-                    <form method="get" action="download/AR DanceSABAH.zip" >
+                    <form method="get" action="download/AR.LKNS.apk" >
                             <button type="submit" class="p-2" style=" background-color: DodgerBlue; border: none; color: white">
                             Download APK</button>
                     </form>
@@ -99,8 +99,18 @@
                 <p class="text-hide"></p>
             </div>
         </div>
+</body>
+<!-- Bootstrap JavaScript Libraries -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+</script>
 
-    </body>
+
 </html>
 
 
