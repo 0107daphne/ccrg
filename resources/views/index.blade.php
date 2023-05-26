e250f6409d69b8ab2c3027d015277d7e580be34a
DAPHNE GOH <0107daph@gmail.com>
2023-05-26 16:04:47 +0800

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>AR-community</title>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <!-- CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <!-- Bootstrap CSS v5.2.1 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="p-3 mb-2 bg-light text-gray d-flex">
+                <div class="w-50">
+                    <img src="{{url('/img/creativecomputing.png') }}">
+                    <img src="" height="60" alt="">
+                    <img src="creativecomputing.jpeg" height="80" alt="">
+                </div>
+                <div class="flex-fill">
+                    <h2 class="font-weight-light">FCI | CCRG</h2>
+                    <h5 class="text-muted">GRANT | PROJECT</h5>
+                </div>
+
+            </div>
+            <div class="bg-dark-subtle font-weight-bold p-3 mb-2 text-black">
+                <h4>AR with Community</h4>
+                <p>Project AR application collaboration with Lembaga Kesenian dan Kebudayaan Negeri Sabah (LKNS)</p>
+            </div>
+            <hr>
+            <div class="p-3 mb-2">
+                <h4>AR DanceSabah</h4>
+                <p><a href="https://drive.google.com/file/d/1X3iN1Z60m7Ohk1YTdaQ7GDv7gORlfiJv/view" class="text-secondary">ARdance Privacy Policy</a></p>
+                <p>Youtube link: <a href="https://www.youtube.com/@lkns.project" class="text-muted">youtube.com/@lkns.project</a></p>
+                <form method="get" action="AR DanceSABAH.zip" >
+                        <button type="submit" class="p-2" style=" background-color: DodgerBlue; border: none; color: white">
+                        <i class="fa fa-download"></i> Download APK</button>
+                </form>
+
+
+            </div>
+            <hr>
+            <div class="p-3 mb-2">
+                <h4>AR art</h4>
+                <form method="get" action="" >
+                        <button type="submit" class="p-2" style=" background-color: DodgerBlue; border: none; color: white">
+                        <i class="fa fa-download"></i> Download APK</button>
+                </form>
+            </div>
+            <hr>
+            <div class="p-3 mb-2">
+                <h4>Kaamatan Gameplay</h4>
+                <p>Kaamatan games link: <a href="http://mydiabetic-gamification.com/play" class="text-muted">mydiabetic-gamification.com/play</a></p>
+            </div>
+            <hr>
+            <div class="p-3 mb-2">
+                <h4>Antanom AR Storytelling book</h4>
+                    <form method="get" action="" >
+                        <button type="submit" class="p-2 mb-2" style=" background-color: DodgerBlue; border: none; color: white;">
+                        <i class="fa fa-download"></i> Download APK</button>
+                    </form>
+
+                    <form method="get" action="" >
+                        <button type="submit" class="p-2" style=" background-color: DodgerBlue; border: none; color: white">
+                        <i class="fa fa-download"></i> Download Storybook</button>
+                    </form>
+            </div>
+            <div class="p-3 mt-2 bg-dark-subtle text-center text-white">
+                <p class="text-hide"></p>
+            </div>
+        </div>
+
+    </body>
+</html>
+
+
